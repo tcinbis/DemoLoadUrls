c5daf7eb51a83263d983e110f0e45dcad89f9cd6
Tom Cinbis <tom.cinbis@unibas.ch>
2017-05-03 15:31:49 +0200

    Improvement of Processing

--- demoURL/src/main/java/Main.java
+++ demoURL/src/main/java/Main.java
@@ -18,9 +18,13 @@ public class Main extends Thread {
     File file = new File("url.db");
     file.delete();
 
-    for (int i = 0; i <= 17;i++){
-      new ReadAndProcessThread(i+"",i*1000000).start();
-    }
+    long totalLines = 170000000;
+    long linesToReadPerThread = totalLines/17;
 
+    /*for (int i = 0; i <= 17;i++){
+      new ReadAndProcessThread(i+"",i*1000000,linesToReadPerThread).start();
+    }*/
+
+    new ReadAndProcessThread("1",0,170000000).start();
   }
 }
--- demoURL/src/main/java/ReadAndProcessThread.java
+++ demoURL/src/main/java/ReadAndProcessThread.java
@@ -14,18 +14,21 @@ public class ReadAndProcessThread extends Thread {
 
   private int startLine = 0;
   private static final String FILENAME = "urls-sample.txt";
-  private static final long BUFFERSIZE = 1000000;
+  private static final long BUFFERSIZE = Integer.MAX_VALUE-10;
+  private static final int COMMITLIMIT = 500000;
+  private long linesToRead;
   private volatile boolean stop = false;
 
-  private UrlDatabase database;
+  private UrlDatabase database; //The database where all data is stored
   private FileChannel fileChannel;
   private CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
   private MappedByteBuffer buffer;
   private CharBuffer charBuffer;
 
-  ReadAndProcessThread(String name, int startLine) {
+  ReadAndProcessThread(String name, int startLine,long linesToRead) {
     this.setName(name);
     this.setStartLine(startLine);
+    this.linesToRead = linesToRead;
   }
 
   public void run() {
@@ -39,21 +42,26 @@ public class ReadAndProcessThread extends Thread {
       //TODO Implement a fast search algorithem to find \n (10 in ascii code)in charBuffer!
       int startIndexOfChar = 0;
       int counterOfUrls = 0;
-      for (int i = 0; i < BUFFERSIZE; i++) {
-        if (charBuffer.get(i) == 10) {
+      int commitCounter = 0;
+      int i = 0;
+      while (commitCounter*COMMITLIMIT <= linesToRead){
+        if (buffer.get(i) == 10) {
           int diff = i - startIndexOfChar;
           char[] tmp = new char[diff];
           for (int j = 0; j < diff; j++) {
-            tmp[j] = charBuffer.get(startIndexOfChar + j);
+            tmp[j] = (char) buffer.get(startIndexOfChar + j);
           }
           database.insert(String.valueOf(tmp));
           startIndexOfChar = i + 1;
           ++counterOfUrls;
         }
-        if (counterOfUrls % 1000000 == 0 && counterOfUrls > 0) {
+        if (counterOfUrls % 100000 == 0 && counterOfUrls > 0) {
           database.commit();
-          System.out.println("Commit");
+          counterOfUrls = 0;
+          ++commitCounter;
+          System.out.println("Commit Counter: "+commitCounter * COMMITLIMIT);
         }
+        ++i;
       }
       database.commit();
       setStop();
@@ -64,8 +72,9 @@ public class ReadAndProcessThread extends Thread {
   private void init() throws IOException {
     database = new UrlDatabase(this.getName());
     fileChannel = new RandomAccessFile(FILENAME, "r").getChannel();
+    //TODO FIND A BETTER WAY TO DETERMINE WHERE TO START READING FROM THE FILE. WE HAVE TO START AT DIFFERENT LINES, NOT AT DIFFERENT POSITIONS IN THE BUFFER
     buffer = fileChannel.map(MapMode.READ_ONLY, startLine, BUFFERSIZE);
-    charBuffer = decoder.decode(buffer);
+    //charBuffer = decoder.decode(buffer);
   }
 
   public void setStartLine(int startLine) {
