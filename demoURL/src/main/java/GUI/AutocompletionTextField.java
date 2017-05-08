a4f4b6e775c7e0563f42d1dfe8f6721b5b65013b
hoepau00 <p.hoeft@stud.unibas.ch>
2017-05-08 13:10:56 +0200

    Finished Reworking to select the right database for the autocompletion

--- demoURL/src/main/java/GUI/AutocompletionTextField.java
+++ demoURL/src/main/java/GUI/AutocompletionTextField.java
@@ -6,9 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import javafx.geometry.Side;
 import javafx.scene.control.ContextMenu;
@@ -31,7 +33,6 @@ public class AutocompletionTextField extends TextField {
   private Statement statement;
   private ResultSet searchResults;
   private ResultSet searchCount;
-  private ArrayList<String> results = new ArrayList<>();
   private boolean animate = false;
   private Animation animation;
 
@@ -111,85 +112,182 @@ public class AutocompletionTextField extends TextField {
   }
 
   private void findOccurences(String enteredText) throws SQLException {
-    searchResults = statement.executeQuery(selectDatabase(enteredText,false)+"LIMIT 5");
-    System.out.println("Search results loaded.");
-    entries.clear();
-    ArrayList<String> sortedResults = new ArrayList<>();
-    while (searchResults != null && searchResults.next()) {
-      sortedResults.add(searchResults.getString(1));
-      System.out.println(searchResults.getString(1));
+    ArrayList<String> databases = selectMatchingDatabases(enteredText);
+    ArrayList<String> result = new ArrayList<>();
+    Character letter = getLetter(enteredText);
+    boolean letterischar = false;
+    if (letter != null) {
+      letterischar = letter.charValue() >= (int) 'a' && letter.charValue() <= (int) 'z';
     }
-    Collections.sort(sortedResults);
+    while (result.size() < 5) {
+      Random ran = new Random();
+      int r = ran.nextInt(databases.size());
+      int letterint = ran.nextInt(26);
+      int offset = ran.nextInt(20);
+      char c = (char) (ran.nextInt(26) + 'a');
+      switch (databases.get(r)) {
+        case HTTP_DATABASE:
+          if (letter == null) {
+            if (letterint == 0) {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTP_DATABASE + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            } else {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTP_DATABASE + "-" + c + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            }
+          } else if (letterischar) {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_DATABASE + "-" + letter + "\" WHERE url LIKE '"
+                    + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          } else {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_DATABASE + "\" WHERE url LIKE '" + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          }
+          break;
+        case HTTP_WWW_DATABASE:
+          if (letter == null){
+            if (letterint == 0 || enteredText.length() <= "http://www.".length()){
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTPS_WWW_DATABASE + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            } else {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTP_WWW_DATABASE + "-" + c + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            }
+          } else if (letterischar){
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_WWW_DATABASE + "-" + letter + "\" WHERE url LIKE '"
+                    + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          } else {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_WWW_DATABASE + "\" WHERE url LIKE '" + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          }
+          break;
+        case HTTPS_DATABASE:
+          if (letter == null) {
+            if (letterint == 0) {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTPS_DATABASE + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            } else {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTPS_DATABASE + "-" + c + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            }
+          } else if (letterischar) {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTPS_DATABASE + "-" + letter + "\" WHERE url LIKE '"
+                    + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          } else {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTPS_DATABASE + "\" WHERE url LIKE '" + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          }
+          break;
+        case HTTPS_WWW_DATABASE:
+          if (letter == null){
+            if (letterint == 0 || enteredText.length() <= "https://www.".length()){
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTPS_WWW_DATABASE + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            } else {
+              ResultSet resultSet = statement.executeQuery(
+                  "SELECT * FROM \"" + HTTP_WWW_DATABASE + "-" + c + "\" WHERE url LIKE '" + enteredText
+                      + "%' LIMIT 1 OFFSET " + offset);
+            }
+          } else if (letterischar){
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_WWW_DATABASE + "-" + letter + "\" WHERE url LIKE '"
+                    + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          } else {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM \"" + HTTP_WWW_DATABASE + "\" WHERE url LIKE '" + enteredText
+                    + "%' LIMIT 1 OFFSET " + offset);
+          }
+          break;
+        case REST_DATABASE:
+          ResultSet resultSet = statement.executeQuery(
+              "SELECT * FROM \"" + REST_DATABASE + "\" WHERE url LIKE '" + enteredText
+                  + "%' LIMIT 1 OFFSET " + offset);
+      }
+      while (searchResults != null && searchResults.next()) {
+        result.add(searchResults.getString(1));
+      }
+    }
+
+    System.out.println("Search results loaded.");
+
+    entries.clear();
+
+    Collections.sort(result);
+    entries.addAll(result);
+  }
+
+  private Character getLetter(String enteredText) {
+    Character letter = null;
+    if (enteredText.startsWith("http://")){
+      if (enteredText.length() > 7) {
+        letter = enteredText.charAt(7);
+      }
+    } if (enteredText.startsWith("http://www.")){
+      if (enteredText.length() > 11) {
+        letter = enteredText.charAt(11);
+      } else {
+        letter = null;
+      }
+    } if (enteredText.startsWith("https://")){
+      if (enteredText.length() > 8) {
+        letter = enteredText.charAt(8);
+      }
+    } if (enteredText.startsWith("https://www.")){
+      if (enteredText.length() > 12) {
+        letter = enteredText.charAt(12);
+      } else {
+        letter = null;
+      }
+    }
+    return letter;
   }
 
   private void countOccurences(String enteredText) throws SQLException {
-    searchCount = statement.executeQuery(selectDatabase(enteredText,true));
+    //searchCount = statement.executeQuery(selectDatabase(enteredText,true));
+    //System.out.println(selectDatabase(enteredText,true));
     System.out.println("Search count done.");
+    /*
     if (searchCount != null && searchCount.next()) {
       System.out.println(searchCount.getString(1));
       AutoSuggestion.displayCount(searchCount.getString(1));
-    }
+    }*/
   }
 
-  private String selectDatabase(String enteredText,boolean count) {
-    boolean https = false;
-    boolean http = false;
-    char firstLetter;
-    if (enteredText.startsWith("https://")) {
-      https = true;
-    } else if (enteredText.startsWith("http://")) {
-      http = true;
+  private ArrayList<String> selectMatchingDatabases(String enteredText) throws SQLException {
+    String Shttps = "https://";
+    String Shttpswww = "https://www.";
+    String Shttp = "http://";
+    String Shttpwww = "http://www.";
+    ArrayList<String>  resultlist = new ArrayList<>();
+    if (enteredText.regionMatches(0,Shttps,0,Math.min(enteredText.length(),Shttps.length())) && !enteredText.startsWith(Shttpswww)) {
+      resultlist.add(HTTPS_DATABASE);
+    } if (enteredText.regionMatches(0,Shttp,0,Math.min(enteredText.length(),Shttp.length())) && !enteredText.startsWith(Shttpwww)) {
+      resultlist.add(HTTP_DATABASE);
+    } if (enteredText.regionMatches(0,Shttpswww,0,Math.min(enteredText.length(),Shttpswww.length()))){
+      resultlist.add(HTTPS_WWW_DATABASE);
+    } if (enteredText.regionMatches(0,Shttpwww,0,Math.min(enteredText.length(),Shttpwww.length()))){
+      resultlist.add(HTTP_WWW_DATABASE);
+    } if (enteredText.length() < Shttp.length() || !(enteredText.startsWith(Shttp)|| enteredText.startsWith(Shttps))){
+      resultlist.add(REST_DATABASE);
     }
-    if (https){
-      if(enteredText.startsWith("www.",8)){
-        firstLetter = enteredText.charAt(12);
-        String query;
-        if (count){
-          query = "SELECT COUNT(*) FROM "+HTTPS_WWW_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        } else {
-          query = "SELECT * FROM "+HTTPS_WWW_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        }
-        return query;
-      } else {
-        firstLetter = enteredText.charAt(8);
-        String query;
-        if (count) {
-          query = "SELECT COUNT(*) FROM "+HTTPS_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        } else {
-          query = "SELECT * FROM "+HTTPS_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        }
-        return query;
-      }
-    } else if (http){
-      if(enteredText.startsWith("www.",7)){
-        firstLetter = enteredText.charAt(11);
-        String query;
-        if (count){
-          query = "SELECT COUNT(*) FROM "+HTTP_WWW_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        } else {
-          query = "SELECT * FROM "+HTTP_WWW_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        }
-        return query;
-      } else {
-        firstLetter = enteredText.charAt(7);
-        String query;
-        if(count){
-          query = "SELECT COUNT(*) FROM "+HTTP_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        } else {
-          query = "SELECT * FROM "+HTTP_DATABASE+"-"+firstLetter+" WHERE url LIKE '"+enteredText+"%' ";
-        }
-        return query;
-      }
-    } else {
-      String query;
-      if (count){
-        query = "SELECT COUNT(*) FROM "+REST_DATABASE+" WHERE url LIKE '"+enteredText+"%' ";
-      } else {
-        query = "SELECT * FROM "+REST_DATABASE+" WHERE url LIKE '"+enteredText+"%' ";
-      }
-      return query;
-    }
-
+    System.out.println(Arrays.toString(resultlist.toArray()));
+    return resultlist;
   }
 
   /**
